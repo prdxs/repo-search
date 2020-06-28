ea5afa2a37b0acaa9089e5e8cd0d995cb633879b
Rubén Fernández <rub.fndz@gmail.com>
2020-06-28 11:34:02 +0100

    fix(ssr): ssr material-ui styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import ThemeProvidersWrapper from '@/components/ThemeProvidersWrapper';
@@ -12,16 +11,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={apolloClient}>
       <ThemeProvidersWrapper>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
         <Component {...pageProps} />
       </ThemeProvidersWrapper>
     </ApolloProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,71 @@
 import React from 'react';
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet as SCServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets as MuiServerStyleSheets } from '@material-ui/core/styles';
+import theme from '@/theme';
 
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
+    const styledComponentSheet = new SCServerStyleSheet();
+    const materialUiSheets = new MuiServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => {
+            const styledApp = styledComponentSheet.collectStyles(
+              <App {...props} />
+            );
+            return materialUiSheets.collect(styledApp);
+          },
         });
 
       const initialProps = await Document.getInitialProps(ctx);
 
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: [
+          ...React.Children.toArray(initialProps.styles),
+          styledComponentSheet.getStyleElement(),
+          materialUiSheets.getStyleElement(),
+        ],
       };
     } finally {
-      sheet.seal();
+      styledComponentSheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
