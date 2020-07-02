97300a88105f8d9181315d8df09269de4efa7f9a
Rubén Fernández <rub.fndz@gmail.com>
2020-07-02 17:15:51 +0100

    fix(repository-search): delete all repo entities when api call fails

--- components/RepositorySearchPage/state/slice.ts
+++ components/RepositorySearchPage/state/slice.ts
@@ -24,6 +24,7 @@ export default createSlice({
     );
     builder.addCase(searchRepositories.rejected, (state) => {
       state.loading = false;
+      repositoriesAdapter.removeAll(state);
     });
   },
 });
