ade0d6a27dee69eb855f1f65a554eeb68a1bff40
Rubén Fernández <rub.fndz@gmail.com>
2020-07-02 17:15:04 +0100

    feat: create repositories page

---
+++ pages/repositories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import RepositorySearchPage from '@/components/RepositorySearchPage';
+
+const RepositoriesPage: React.FC = () => <RepositorySearchPage />;
+
+export default RepositoriesPage;
